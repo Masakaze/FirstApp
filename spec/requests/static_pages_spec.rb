66fc91e23e167d5524027671dee7418fe3038837
Masakaze Sato <Masakaze@Masakaze-no-MacBook-Air.local>
2015-05-01 23:53:30 +0900

    StaticPagesのタイトルを動的に変更できるようapplication.html.erbと各viewを変更

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,17 +6,32 @@ describe "Static pages" do
     it "should have the content 'StaticPagesHome'" do      visit '/static_pages/home'
       expect(page).to have_content('StaticPagesHome')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
   end
 
   describe "Help page" do
     it "should have the content 'StaticPagesHelp'" do      visit '/static_pages/help'
       expect(page).to have_content('StaticPagesHelp')
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end
   end
 
   describe "About page" do
     it "should have the content 'StaticPagesAbout'" do      visit '/static_pages/about'
       expect(page).to have_content('StaticPagesAbout')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About")
     end
   end
 end
