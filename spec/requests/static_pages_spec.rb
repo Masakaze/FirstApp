53d5bfcec5f2f4fe2d5184f6830a721f8d0fb157
Masakaze Sato <Masakaze@Masakaze-no-MacBook-Air.local>
2015-05-02 16:59:28 +0900

    StaticPagesのルーティングをstatic_pagesなしでたどりつくように変更\nテストも変更

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,45 +3,49 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
-    it "should have the content 'StaticPagesHome'" do      visit '/static_pages/home'
+    it "should have the content 'StaticPagesHome'" do      
+      visit root_path
       expect(page).to have_content('StaticPagesHome')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title("Home")
     end
   end
 
   describe "Help page" do
-    it "should have the content 'StaticPagesHelp'" do      visit '/static_pages/help'
+    it "should have the content 'StaticPagesHelp'" do
+      visit help_path
       expect(page).to have_content('StaticPagesHelp')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Help")
     end
   end
 
   describe "About page" do
-    it "should have the content 'StaticPagesAbout'" do      visit '/static_pages/about'
+    it "should have the content 'StaticPagesAbout'" do      
+      visit about_path
       expect(page).to have_content('StaticPagesAbout')
     end
 
     it "should have the title 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("About")
     end
   end
 
   describe "Contact page" do
-    it "should have the content 'StaticPagesContact'" do      visit '/static_pages/contact'
+    it "should have the content 'StaticPagesContact'" do
+      visit contact_path
       expect(page).to have_content('StaticPagesContact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("Contact")
     end
   end
