321670aca3f181585ee20285c8c0eef0a0284ec1
Masakaze Sato <Masakaze@Masakaze-no-MacBook-Air.local>
2015-05-03 00:41:35 +0900

    演習5に従ってshared_examples_forを使って同一処理をまとめた

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,28 +4,42 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_title(page_title) }
+    it { should have_content(heading) }
+  end
+
   describe "Home page" do
     before { visit root_path }
-    it { should have_content("StaticPagesHome") }
-    it { should have_title("") }
+    let(:heading) 		{ "StaticPagesHome" }
+    let(:page_title) 	{ "" }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title("Home") }
   end
 
   describe "Help page" do
     before { visit help_path }
-    it { should have_content('StaticPagesHelp') }
-    it { should have_title("Help") }
+    let(:heading) 		{ "StaticPagesHelp" }
+    let(:page_title) 	{ "Help" }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
-    it { should have_content('StaticPagesAbout') }
-    it { should have_title("About") }
+    let(:heading) 		{ "StaticPagesAbout" }
+    let(:page_title) 	{ "About" }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    it { should have_content('StaticPagesContact') }
-    it { should have_title("Contact") }
+    let(:heading) 		{ "StaticPagesContact" }
+    let(:page_title) 	{ "Contact" }
+
+    it_should_behave_like "all static pages"
   end
 end
 
