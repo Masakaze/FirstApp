aa9f86c41772d7a43865fcc0fc1b88328f3d7174
Masakaze Sato <Masakaze@Masakaze-no-MacBook-Air.local>
2015-05-03 15:36:33 +0900

    Userのemailアドレスの登録を一意性を持たせるようにした。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
   has_many :microposts
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,4 +23,34 @@ describe User do
     before { @user.name = "a"*51 }
     it { should_not be_valid }
   end
+
+  describe "when email is invalid format" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses.each { |invalid_addr|
+        @user.email = invalid_addr
+        expect(@user).not_to be_valid
+      }
+    end
+  end
+
+  describe "when email is valid format" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each { |valid_addr|
+        @user.email = valid_addr
+        expect(@user).to be_valid
+      }
+    end
+  end
+
+  describe "when email address is already taken" do
+    before do
+      same_email_user = @user.dup
+      same_email_user.email = @user.email.downcase
+      same_email_user.save
+    end
+
+    it { should_not be_valid }
+  end
 end
