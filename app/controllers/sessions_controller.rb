a04361cc64878faf87cd635beb383d35be136a96
Masakaze Sato <Masakaze@Masakaze-no-MacBook-Air.local>
2015-05-05 17:32:20 +0900

    ユーザー認証の成否で処理を分ける準備。flashハッシュに成否メッセージをいれapplication.html.erbで表示するようにした。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,13 @@ class SessionsController < ApplicationController
 
   def create
 
+    if false
+      # ユーザー認証成功
+    else
+      # ユーザー認証失敗
+      flash[:error] = "Authentication failed"
+      render 'new'
+    end
   end
 
   def destroy
