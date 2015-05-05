637dc4995269ae828e86a76e222635f34e161232
Masakaze Sato <Masakaze@Masakaze-no-MacBook-Air.local>
2015-05-05 17:40:02 +0900

    Sign inの簡易テストしてみた2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
 
-    if false
+    if user != nil && user.authenticate(params[:session][:password])
       # ユーザー認証成功
+      flash[:success] = "Authentication success"
+      render 'new'
     else
       # ユーザー認証失敗
       flash[:error] = "Authentication failed"
